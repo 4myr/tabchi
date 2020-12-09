d59db298d9f413ea84352a9ba795f0b1d98111a4
AMYR <awp.1379@gmail.com>
2020-12-10 00:28:52 +0330

    Add bot configs and welcome

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import config
 import redis
 import sys, os, asyncio
 
-# A simply function for showing errors
-def client_has_error(error):
-    print("[Error]: ", error)
-
-# Initialize Redis-Server
-r = redis.Redis()
-if not r.ping():
-    client_has_error("Redis is not ready!")
-    exit();
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # Handle multiple instances without any duplicate
 # instance = input('Enter your instance name [example: amyr]: ') if len(sys.argv) < 2 else sys.argv[1]
@@ -23,6 +24,32 @@ instance = "amyr"
 instance_path = os.path.abspath("Sessions/{0}".format(instance))
 
 
+# A simply function for showing errors
+def client_has_error(error):
+    print(colors.FAIL + "[Error]: " + colors.ENDC + error)
+
+# Join redis variable name to instance name
+def cnf(name):
+    return "{0}_{1}".format(instance, name)
+
+
+# Initialize Redis-Server
+r = redis.Redis()
+if not r.ping():
+    client_has_error("Redis is not ready!")
+    exit();
+
+# Initilize variables
+CRON_TIME = int( r.get( cnf("CRON_TIME") ) ) or ( 120 and r.set( cnf("CRON_TIME"), 120 ) and print("CRON_TIME has been set to default (120)") )
+MAX_GROUPS = int( r.get( cnf("MAX_GROUPS") ) ) or ( 150 and r.set( cnf("MAX_GROUPS"), 150 ) and print("MAX_GROUPS has been set to default (150)") )
+BOT_USER = r.get( cnf("BOT_USER") ) or None # configured BOT_USER will replace on adverstiment texts for {BOT_USER}
+
+welcome_text = "------ WELCOME ------\n"
+welcome_text += "Redis:         " + colors.OKGREEN + "is ok!" + colors.ENDC + "\n"
+welcome_text += "CRON_TIME:     " + colors.UNDERLINE + str(CRON_TIME) + colors.ENDC + "\n" 
+welcome_text += "MAX_GROUPS:    " + colors.UNDERLINE + str(MAX_GROUPS) + colors.ENDC + "\n" 
+welcome_text += "BOT_USER:      " + colors.UNDERLINE + str(BOT_USER) + colors.ENDC + "\n\n" 
+print(welcome_text)
 
 # A simply function to making link for a user
 def make_user_link(from_id, name):
@@ -75,22 +102,22 @@ async def newMessage(event):
         response_text = "**My Stats**\n\nUsers: `{}`\nGroups: `{}`\nChannels: `{}`\nBots: `{}`\nAll: `{}`\n\n**RAM Usage: %{}**\n**CPU Usage: %{}**\n**Disk Usage: %{}**".format(count_users, count_groups, count_channels, count_bots, count_all, psutil.virtual_memory()[2], psutil.cpu_percent(), psutil.disk_usage('/')[3])
         await event.reply(response_text)
 
-# Create timer event to handling tasks need timer
-def create_timer_event():    
+# Create cron event
+def create_cron_event():    
     loop = asyncio.get_event_loop()
-    timer_task = timer(2)
-    task = loop.create_task(timer_task)
+    cron_task = cron(2)
+    task = loop.create_task(cron_task)
     loop.run_until_complete(task)
 
-# All codes need timer write here
-async def timer(time):
+# All codes need cron write here
+async def cron(time):
     while True:
         print("Task working!")
         await asyncio.sleep(time)
 
 try:
     client.start()
-    task = create_timer_event()
+    task = create_cron_event()
     client.run_until_disconnected()
 except Exception as error:
     client_has_error(error)
